ca07e92a355a2e254b4c37d89ab0499321f0ebf5
Paavo Karppinen <paavo_karppinen@outlook.com>
2024-09-22 12:03:49 +0300

    Add sensor name updating and cascading to related items

--- IoTCloud/Services/SensorsService.cs
+++ IoTCloud/Services/SensorsService.cs
@@ -19,6 +19,7 @@ namespace IoTCloud.Services
 
             if (existingSensor is null)
             {
+                sensor.CreatedAt = DateTimeOffset.Now;
                 await context.Sensors.AddAsync(sensor);
                 await context.SaveChangesAsync();
                 return true;
@@ -83,6 +84,124 @@ namespace IoTCloud.Services
             var sensors = await context.Sensors.Where(s => s.UserId == userId).ToListAsync();
 
             return sensors;
+        }
+
+        public async Task<bool> EditSensor(Sensor sensor, string oldSensorName)
+        {
+            var dbSensor = await context.Sensors.FindAsync(sensor.Id);
+
+            if (dbSensor is not null)
+            {
+                using (var connection = GetConnection())
+                {
+                    connection.Open();
+
+                    using (var transaction = connection.BeginTransaction())
+                    {
+                        try
+                        {
+                            var updateSensorSql = @"
+                                                  UPDATE Sensors
+                                                  SET Name = @SensorName, ModifiedAt = @ModifiedAt
+                                                  WHERE Id = @Id";
+
+                            var sensorUpdated = await connection.ExecuteAsync(updateSensorSql, new { sensor.Id, SensorName = sensor.Name, ModifiedAt = DateTimeOffset.Now }, transaction);
+
+                            var updateDistanceReadingSql = @"
+                                                           UPDATE DistanceReadings
+                                                           SET SensorName = @SensorName
+                                                           WHERE UserId = @UserId
+                                                           AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateDistanceReadingSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+                            var updateHumidityReadingSql = @"
+                                                           UPDATE HumidityReadings
+                                                           SET SensorName = @SensorName
+                                                           WHERE UserId = @UserId
+                                                           AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateHumidityReadingSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+                            var updateLuminosityReadingSql = @"
+                                                             UPDATE LuminosityReadings
+                                                             SET SensorName = @SensorName
+                                                             WHERE UserId = @UserId
+                                                             AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateLuminosityReadingSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+                            var updateTemperatureReadingSql = @"
+                                                              UPDATE TemperatureReadings
+                                                              SET SensorName = @SensorName
+                                                              WHERE UserId = @UserId
+                                                              AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateTemperatureReadingSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+                            var updateVelocityReadingSql = @"
+                                                           UPDATE VelocityReadings
+                                                           SET SensorName = @SensorName
+                                                           WHERE UserId = @UserId
+                                                           AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateVelocityReadingSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+                            var updateBinaryReadingSql = @"
+                                                         UPDATE BinaryReadings
+                                                         SET SensorName = @SensorName
+                                                         WHERE UserId = @UserId
+                                                         AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateBinaryReadingSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+                            var updateGraphItemsSql = @"
+                                                      UPDATE GraphItems
+                                                      SET SensorName = @SensorName
+                                                      WHERE UserId = @UserId
+                                                      AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateGraphItemsSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+                            var updateTablesSql = @"
+                                                  UPDATE TableItems
+                                                  SET SensorName = @SensorName
+                                                  WHERE UserId = @UserId
+                                                  AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateTablesSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+                            var updateBinaryGraphsSql = @"
+                                                        UPDATE BinaryGraphItems
+                                                        SET SensorName = @SensorName
+                                                        WHERE UserId = @UserId
+                                                        AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateBinaryGraphsSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+
+                            var updateEmailNotificationsSql = @"
+                                                              UPDATE EmailNotifications
+                                                              SET SensorName = @SensorName
+                                                              WHERE UserId = @UserId
+                                                              AND SensorName = @OldSensorName";
+
+                            await connection.ExecuteAsync(updateEmailNotificationsSql, new { sensor.UserId, SensorName = sensor.Name, OldSensorName = oldSensorName }, transaction);
+
+
+                            transaction.Commit();
+
+                            return sensorUpdated > 0;
+                        }
+                        catch (Exception ex)
+                        {
+                            transaction.Rollback();
+                            return false;
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
